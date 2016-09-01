3e398623dafef0d11f951eb6c542a010c3596c8b
Kyungho Oh <kyungho.oh@jaeguklee-HP-Compaq-Elite-8300-MT.LGE.NET>
2016-09-01 10:00:47 +0900

    First Upload

---
+++ mkcscope.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf cscope.out cscope.files
+find `pwd` -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' > cscope.files
+cscope -i cscope.files
